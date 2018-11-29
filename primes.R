acbc7fad39b0bfb81ab54e728b04be7c7160909e
Bestest96 <jaluki@vp.pl>
2018-11-29 23:24:09 +0100

    Updated primes.R with some vectorization, added first results

--- primes.R
+++ primes.R
@@ -8,8 +8,6 @@ if (!require("sfsmisc")) {
 
 dist_cities <- function(x1, y1, x2, y2) {
   dist <- sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
-  if (dist < 0 || is.complex(dist))
-    stop("Wrong dist!")
   return (dist)
 }
 
@@ -100,8 +98,9 @@ order <- integer(dim(cities)[1] + 1)
 # order[dim(cities)[1] + 1] <- 0
 # order[1:dim(cities)[1]] <- cities[,"CityId"]
 
-checked_neighbours <- 10000
+checked_neighbours <- length(cities[,"CityId"])
 path_length <- 0
+paths <- double(length(order) - 1)
 
 for (i in 1:(length(order) - 2)) {
   to_check <- setdiff(1:max(cities[,"CityId"]), order)
@@ -111,30 +110,28 @@ for (i in 1:(length(order) - 2)) {
     neighbours <- to_check
   min_dist <- Inf
   min_neigh <- NA
-  for (n in neighbours) {
-    neigh_dist <- dist_cities(cities[order[i] + 1, 'X'],
-                              cities[order[i] + 1, 'Y'],
-                              cities[n + 1, 'X'],
-                              cities[n + 1, 'Y'])
-    check_dist <- neigh_dist
-    if (i %% 10 == 0)
-      if (!(n %in% city_primes)) {
-        neigh_dist <- 1.1 * neigh_dist
-        check_dist <- neigh_dist
-      }
-    else
-      if (n %in% city_primes)
-        check_dist <- 1.1 * check_dist
-    if (check_dist < min_dist) {
-      min_neigh <- n
-      min_dist <- neigh_dist
-    }
+  x1 <- cities[order[i] + 1, 'X']
+  y1 <- cities[order[i] + 1, 'Y']
+  x2 <- cities[neighbours + 1, 'X']
+  y2 <- cities[neighbours + 1, 'Y']
+  dists <- dist_cities(x1, y1, x2, y2)
+  if (i %% 10 == 0) {
+    check_dists <- ifelse(neighbours %in% city_primes, dists, 1.1 * dists)
+    dists <- check_dists
   }
+  else
+    check_dists <- ifelse(neighbours %in% city_primes, 1.1 * dists, dists)
+  min_indx <- which.min(check_dists)
+  min_dist <- dists[min_indx]
+  min_neigh <- neighbours[min_indx]
   order[i + 1] <- min_neigh
   path_length <- path_length + min_dist
+  paths[i] <- min_dist
   cat("\r", i, ",", path_length)
 }
-
+last_len <- dist_cities(cities[order[i + 1] + 1, 'X'], cities[order[i + 1] + 1, 'Y'], cities[1, 'X'], cities[1, 'Y'])
+paths[i + 1] <- last_len
+path_length <- path_length + last_len
 
 # to check numerical errors
 # changes <- sample(100:120000, 90000)
