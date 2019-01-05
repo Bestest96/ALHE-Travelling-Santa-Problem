c2c6fabd9557a6f29f70dd773977f325c33cbfae
Rafał Wójcik <ravw96@gmail.com>
2019-01-05 01:44:06 +0100

    Custom clustering concentrated on prime cities.

--- clustering.R
+++ clustering.R
@@ -1,5 +1,16 @@
+if (!require("sfsmisc")) {
+  print("Sfsmisc not found! Installing.")
+  install.packages("sfsmisc")
+  if (!require("sfsmisc")) {
+    stop("Cannot install ")
+  }
+}
+
+printf <- function(...) invisible(print(sprintf(...)))
+
 dist_cities <- function(x1, y1, x2, y2) {
   dist <- sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
+  
   return (dist)
 }
 
@@ -45,10 +56,122 @@ clusterify <- function(X, radius, metric = dist_cities) {
   return (clusters)
 }
 
+"
+Removes n furthest points from a cluster.
+"
+trim_cluster <- function(n, 
+                         group, 
+                         points, 
+                         dists, 
+                         inverted = FALSE, 
+                         return_dists = FALSE){
+  if(n > length(points)) stop("n can't be greater than length of points.")
+  sorted_by_dist_idxs <- order(dists, decreasing = TRUE)
+  to_remove <- c()
+  for(idx in sorted_by_dist_idxs) {
+    if(n == 0) break
+    point <- points[idx]
+    if((!inverted && point %in% group) || (inverted && !(point %in% group))){
+      n <- n - 1
+      to_remove <- c(to_remove, idx)
+    }
+  }
+  
+  if(return_dists){
+    rv <- list(points[-to_remove], dists[-to_remove])
+    return (rv)
+  }
+  return (points[-to_remove])
+}
+
+"
+Clusterifies points from X.
+
+Args:
+  X: A list of points. 
+  radius: Base radius of a cluster.
+  metric: Metric to calculate distance between points.
+  attractors: A list of indices of points from X
+  on which clusters would be concentrated.
+  per_attractor: Number of normal points per one attractor.
+
+Returns:
+  A list of length equal number of points containing
+  integers which indicate cluster membership of points.
+"
+clusterify_around_attractors <- function(X, 
+                                         radius,
+                                         attractors,
+                                         per_attractor,
+                                         metric = dist_cities) {
+  n_cluster <- 1
+  clusters <- vector(length=nrow(X))
+  remaining_idxs <- 1:nrow(X)
+  remaining_attractors <- attractors
+  total_attractors <- length(attractors)
+  while(length(remaining_attractors) > 0) {
+    s <- sample(length(remaining_attractors), 1)
+    idx <- remaining_attractors[s]
+    core <- X[idx,]
+    dists <- metric(cities[remaining_idxs,1], 
+                    cities[remaining_idxs,2], 
+                    core[[1]], 
+                    core[[2]])
+    
+    neighbours_idxs <- remaining_idxs[dists <= radius]
+    n_attractors <- length(intersect(neighbours_idxs, attractors))
+    neighbourhood_size <- length(neighbours_idxs)
+    n_normal_points <- neighbourhood_size - n_attractors
+    neighbours_dists <- dists[dists <= radius]
+    
+    n_remove <- n_normal_points - per_attractor * n_attractors
+    if(n_remove >= 0) inverted <- TRUE # removing normal points
+    # removing attractors
+    else{
+      inverted <- FALSE
+      mod <- n_normal_points %% per_attractor
+      n_remove <- n_attractors - (n_normal_points - mod) / per_attractor
+      rv <- trim_cluster(mod,
+                         attractors,
+                         neighbours_idxs,
+                         neighbours_dists,
+                         return_dists = TRUE,
+                         inverted = TRUE)
+      neighbours_idxs <- rv[[1]]
+      neighbours_dists <- rv[[2]]
+    }
+    neighbours_idxs <- trim_cluster(n_remove,
+                                    attractors,
+                                    neighbours_idxs,
+                                    neighbours_dists,
+                                    inverted = inverted)
+    
+    clusters[neighbours_idxs] <- n_cluster
+    n_cluster <- n_cluster +  1
+    
+    cluster_attractors <- intersect(neighbours_idxs, attractors)
+    remaining_attractors <- setdiff(remaining_attractors, cluster_attractors)
+    n_attractors <- length(cluster_attractors)
+    neighbourhood_size <- length(neighbours_idxs)
+    n_normal_points <- neighbourhood_size - n_attractors
+    remaining_idxs <- setdiff(remaining_idxs, neighbours_idxs)
+    printf("total_attractors = %d, attractors = %d, normal points = %d", 
+           length(remaining_attractors),
+           n_attractors, 
+           n_normal_points)
+  }
+  clusters[remaining_idxs] <- 0
+  
+  print(length(remaining_idxs))
+  
+  return (clusters)
+}
+
 cities <- read.csv("cities.csv", header = T)
 cities <- cities[,2:3]
+city_primes <- primes(dim(cities)[1] - 1)
 
-clusters <- clusterify(cities, 600, max_metric)
+clusters <- clusterify_around_attractors(cities, 500, city_primes, 9)
 
 plot(cities, col=clusters)
-points(cities[clusters==0,], pch = 3, col = "grey")
+# points(cities[clusters==0,], pch = 3, col = "grey")
