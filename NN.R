3dc4b7e82a71dd46f3bdf5115075452dcb9d4e29
Bestest96 <jaluki@vp.pl>
2019-01-13 01:06:10 +0100

    Removed else because someone was complaining...

--- NN.R
+++ NN.R
@@ -6,8 +6,7 @@ find.city <- function(x, y, eps = 1e-30) {
   toRet <- intersect(which(abs(cities[,][[1]] - x) <= eps), which(abs(cities[,][[2]] - y) <= eps)) - 1
   if (length(toRet) == 1)
     return (toRet)
-  else
-    return (NULL)
+  return (NULL)
 }
 
 NN <- function(clusters, c.order, dt) {
