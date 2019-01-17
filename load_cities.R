5df5f986a26c53ade97b829980ab45a529839075
llepak <jaluki@vp.pl>
2019-01-17 01:39:14 +0100

    Changed random walk to best walk, changed pathLength

--- load_cities.R
+++ load_cities.R
@@ -15,71 +15,17 @@ dist_cities <- function(x1, y1, x2, y2) {
   return (dist)
 }
 
-pathLength <- function(ord, c = cities, c_p = city_primes, toChange = NULL, old_length = NULL, old_paths = NULL) {
-  len <- 0.0
-  if (is.null(toChange)) {
-    paths <- double(length(ord) - 1)
-    for (i in 1:(length(ord) - 1)) {
-      dist <- dist_cities(c[ord[i + 1] + 1, 'X'], c[ord[i + 1] + 1, 'Y'], 
-                          c[ord[i] + 1, 'X'], c[ord[i] + 1, 'Y'])
-      if (i %% 10 == 0 && !(ord[i] %in% c_p)) {
-        dist <- 1.1 * dist
-      }
-      len <- len + dist
-      paths[i] <- dist
-    }
-  }
-  else {
-    if (is.null(old_length) || is.null(old_paths))
-      stop("No old sth.")
-    else if (length(toChange) %% 2 != 0)
-      stop("No even length of toChange.")
-    i <- 1
-    len <- old_length
-    paths <- old_paths
-    while (i <= length(toChange)) {
-      c1 <- c[ord[toChange[i]] + 1,]
-      p1 <- c[ord[(toChange[i] - 1)] + 1,]
-      n1 <- c[ord[(toChange[i] + 1)] + 1,]
-      c2 <- c[ord[toChange[i + 1]] + 1,]
-      p2 <- c[ord[(toChange[i + 1] - 1)] + 1,]
-      n2 <- c[ord[(toChange[i + 1] + 1)] + 1,]
-      
-      p1.c1 <- old_paths[toChange[i] - 1]
-      p2.c2 <- old_paths[toChange[i + 1] - 1]
-      c1.n1 <- old_paths[toChange[i]]
-      c2.n2 <- old_paths[toChange[i + 1]]
-      
-      p1.c2 <- dist_cities(c2[,'X'], c2[,'Y'], p1[,'X'], p1[,'Y'])
-      p2.c1 <- dist_cities(c1[,'X'], c1[,'Y'], p2[,'X'], p2[,'Y'])
-      c2.n1 <- dist_cities(n1[,'X'], n1[,'Y'], c2[,'X'], c2[,'Y'])
-      c1.n2 <- dist_cities(n2[,'X'], n2[,'Y'], c1[,'X'], c1[,'Y'])
-      
-      if (toChange[i] %% 10 == 0 && !(ord[toChange[i + 1]] %in% c_p))
-        c2.n1 <- 1.1 * c2.n1
-      else if (((toChange[i] - 1)) %% 10 == 0 && !(ord[((toChange[i] - 1))] %in% c_p))
-        p1.c2 <- 1.1 * p1.c2
-      if (toChange[i + 1] %% 10 == 0 && !(ord[toChange[i]] %in% c_p))
-        c1.n2 <- 1.1 * c1.n2
-      else if (((toChange[i + 1] - 1)) %% 10 == 0 && !(ord[((toChange[i + 1] - 1))] %in% c_p))
-        p2.c1 <- 1.1 * p2.c1
-      len <- len - p1.c1 - p2.c2 - c1.n1 - c2.n2 + p1.c2 + p2.c1 + c2.n1 + c1.n2
-      p1.c2 -> paths[toChange[i] - 1]
-      p2.c1 -> paths[toChange[i + 1] - 1]
-      c2.n1 -> paths[toChange[i]]
-      c1.n2 -> paths[toChange[i + 1]]
-      tmp <- ord[toChange[i]]
-      ord[toChange[i]] <- ord[toChange[i + 1]]
-      ord[toChange[i + 1]] <- tmp
-      i <- i + 2
-    }
-  }
-  if (len < 0) {
-    write.table("Error, len less than 0!", "log.txt", append = T, quote = F, row.names = F, col.names = F)
-    write.table(toChange, "log.txt", append = T, quote = F, row.names = F, col.names = F)
-    write.table(len, "log.txt", append = T, quote = F, row.names = F, col.names = F)
-    stop("Error, len less than 0!")
-  }
+pathLength <- function(order, c = cities, cp = city_primes) {
+  x1 <- order[1:(length(order) - 1)]
+  x2 <- order[2:length(order)]
+  paths <- dist_cities(c[x1 + 1, 'X'],
+                       c[x1 + 1, 'Y'],
+                       c[x2 + 1, 'X'],
+                       c[x2 + 1, 'Y'])
+  inds.to.penalize <- x1[!(x1 %in% cp)]
+  inds.to.penalize <- inds.to.penalize[inds.to.penalize %% 10 == 0]
+  paths[inds.to.penalize] <- 1.1 * paths[inds.to.penalize]
+  len <- sum(paths)
   return (list("length" = len, "paths" = paths))
 }
 
---
+++ walks.R
@@ -0,0 +1,58 @@
+source("NN.R")
+source("load_cities.R")
+
+cluster_random_walk <- function(clusters, first_cluster, dt, iterations = 200) {
+  best_length <<- Inf
+  best_corder <<- NULL
+  best_order <<- NULL
+  counter <- 1
+  while(counter <= iterations) {
+    new_corder <- c(first_cluster, setdiff(sample(max(clusters)), first_cluster))
+    new_order <- NN(clusters, new_corder, dt)
+    new_length <- pathLength(new_order, cities, city_primes)$length
+    if(new_length < best_length) {
+      best_length <<- new_length
+      best_corder <<- new_corder
+      best_order <<- new_order
+    }
+    print.info(c(new_length, best_length))
+    counter <- counter + 1
+  }
+  
+  return (list(order = best_order, corder = best_corder, length = best_length))
+}
+
+best_walk <- function(order) {
+  path_obj <- pathLength(order)
+  best_order <<- order
+  best_length <<- path_obj$length
+  cur_order <<- best_order
+  cur_length <<- best_length
+  to_sample <- 2:(length(order)-2)
+  checked <- c()
+  i <- 1
+  while(T) {
+    if (length(to_sample) == 0)
+      stop("Nothing better to be found.")
+    to_change <- sample(to_sample, 1)
+    to_change <- c(to_change, to_change + 1)
+    cur_order[to_change] <<- cur_order[rev(to_change)]
+    path_obj <- pathLength(cur_order)
+    cur_length <<- path_obj$length
+    if(cur_length < best_length) {
+      best_length <<- cur_length
+      best_order <<- cur_order
+      to_sample <- 2:(length(order)-2)
+      print()
+      print.info(c("Iteration =", i, "best length = ", best_length))
+    }
+    else {
+      cur_length <<- best_length
+      cur_order <<- best_order
+      to_sample <- to_sample[-(to_change - 1)]
+    }
+    
+    cat("\r", i, cur_length)
+    i <- i + 1
+  }
+}
